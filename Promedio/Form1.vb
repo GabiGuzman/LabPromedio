c09788a2f1ee53cf21f086bda85f3594334ae6eb
Guzman <Guzman@DESKTOP-29GVUO5>
2022-03-08 08:30:28 -0600

    Agregar archivos de proyecto.

---
+++ Promedio/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a As Double
+        Dim b As Double
+        Dim c As Double
+        Dim d As Double
+        Dim f As Double
+        Dim r As Double
+
+        a = TextBox1.Text
+        b = TextBox2.Text
+        c = TextBox3.Text
+        d = TextBox4.Text
+
+        f = a + b + c + d
+        r = f / 4
+        TextBox5.Text = r
+
+
+    End Sub
+End Class
